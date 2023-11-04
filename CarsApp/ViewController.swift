3b08f8a46cfe1c1f2eba81c801656f880692e36b
vyatkinsen <vyatkinsen@bk.ru>
2023-11-04 18:55:53 +0300

    Initial commit

--- CarsApp/ViewController.swift
+++ CarsApp/ViewController.swift
@@ -1,19 +1,146 @@
-//
-//  ViewController.swift
-//  CarsApp
-//
-//  Created by Арсений Вяткин on 03.11.2023.
-//
-
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    
+    var context: NSManagedObjectContext!
+    var car: Car!
+    let defaults = UserDefaults.standard
+    lazy var dateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateStyle = .short
+        df.timeStyle = .none
+        return df
+    }()
+    
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var markLabel: UILabel!
+    @IBOutlet weak var modelLabel: UILabel!
+    @IBOutlet weak var carImageView: UIImageView!
+    @IBOutlet weak var lastTimeStartedLabel: UILabel!
+    @IBOutlet weak var numberOfTripsLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var myChoiceImageView: UIImageView!
+    
+    @IBAction func segmentedCtrlPressed(_ sender: UISegmentedControl) {
+        
+    }
+    
+    @IBAction func startEnginePressed(_ sender: UIButton) {
+        car.timesDriven += 1
+        car.lastStarted = Date()
+        
+        do {
+            try context.save()
+            insertDataFrom(selectedCar: car)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+    
+    @IBAction func rateItPressed(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "rate it", message: "Rate this car", preferredStyle: .alert)
+        let rateAction = UIAlertAction(title: "Rate", style: .default) { action in
+            if let text = alertController.textFields?.first?.text {
+                self.update(rating: (text as NSString).doubleValue)
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default)
+        
+        alertController.addTextField() { textField in
+            textField.keyboardType = .numberPad
+        }
+        
+        alertController.addAction(rateAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        getDataFromFile()
+        
+        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+        let mark = segmentedControl.titleForSegment(at: 0)
+        fetchRequest.predicate = NSPredicate(format: "mark == %@", mark!)
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            car = results.first
+            insertDataFrom(selectedCar: car!)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
     }
+    
+    private func getDataFromFile() {
+        if defaults.bool(forKey: "firstLaunch") {
 
+        } else {
+            guard let pathToFile = Bundle.main.path(forResource: "data", ofType: "plist"), let dataArrary = NSArray(contentsOfFile:  pathToFile) else { return }
+            
+            for dictionary in dataArrary {
+                let entity = NSEntityDescription.entity(forEntityName: "Car", in: context)
+                let car = NSManagedObject(entity: entity!, insertInto: context) as! Car
+                
+                let carDictionary = dictionary as! [String: AnyObject]
+                car.mark = carDictionary["mark"] as? String
+                car.model = carDictionary["model"] as? String
+                car.rating = carDictionary["rating"] as! Double
+                car.lastStarted = carDictionary["lastStarted"] as? Date
+                car.timesDriven = carDictionary["timesDriven"] as! Int16
+                car.myChoice = carDictionary["myChoice"] as! Bool
+                
+                let imageName = carDictionary["imageName"] as? String
+                let image = UIImage(named: imageName!)
+                let imageData = image!.pngData()
+                car.imageData = imageData
+                
+                if let colorDictionary = carDictionary["tintColor"] as? [String : Float] {
+                    car.tintColor = getColor(colorDictionary: colorDictionary)
+                }
+            }
+            defaults.set(true, forKey: "First Launch")
+        }
+    }
+    
+    private func getColor(colorDictionary: [String : Float]) -> UIColor {
+        guard let red = colorDictionary["red"],
+              let green = colorDictionary["green"],
+              let blue = colorDictionary["blue"] 
+        else { return UIColor() }
+        
+        return UIColor(red: CGFloat(red / 255), green: CGFloat(green / 255), blue: CGFloat(blue / 255), alpha: 1.0)
+    }
+    
+    private func insertDataFrom(selectedCar car: Car) {
+        carImageView.image = UIImage(data: car.imageData!)
+        markLabel.text = car.mark
+        modelLabel.text = car.model
+        myChoiceImageView.isHidden = !(car.myChoice)
+        ratingLabel.text = "Rating: \(car.rating) / 10"
+        numberOfTripsLabel.text = "Number of trips: \(car.timesDriven)"
+        
+        lastTimeStartedLabel.text = "Last time started: \(dateFormatter.string(from: car.lastStarted!))"
+        segmentedControl.backgroundColor = car.tintColor as? UIColor
+    }
+    
+    private func update(rating: Double) {
+        car.rating = rating
+        
+        do {
+            try context.save()
+            insertDataFrom(selectedCar: car)
+        } catch let error as NSError {
+            let alertController = UIAlertController(title: "Wrong value", message: "Wrong input", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Ok", style: .default)
+            alertController.addAction(okAction)
+            present(alertController, animated: true, completion: nil)
+            print(error.localizedDescription)
 
+        }
+    }
 }
 
